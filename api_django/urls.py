986419b6239f22f7f11f29cb48894002cd791a35
Ian Jabriel <ianjabriel23@gmail.com>
2024-10-14 17:38:08 -0300

    CRUD feito

--- api_django/urls.py
+++ api_django/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('users/',views.get_users,name='get_all_users'),
-   path('courses/',views.get_courses,name='get_all_courses'),
-   path('areas/',views.get_areas,name='get_all_areas'),
-   path('organizations/',views.get_organizations,name='get_all_organizations'),
-   path('data',views.user_manager),
+   path('users/',views.user_manager),
+   path('courses/',views.course_manager),
+   path('areas/',views.area_manager),
+   path('organizations/',views.organization_manager),
 ]
