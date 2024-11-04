04ae909a1af58ca05a819537863e34568cbb3fd7
Ian Jabriel <ianjabriel23@gmail.com>
2024-11-04 15:02:34 -0300

    template

--- api_django/serializers.py
+++ api_django/serializers.py
@@ -5,17 +5,22 @@ from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     cpf = serializers.CharField(required=True)
+    ensino_medio = serializers.BooleanField(read_only=True)
     class Meta:
         model = User
         fields = ['cpf','name','id_course','email','ra','password','ensino_medio']
         #fields = "__all__" 
 
     def create(self, validated_data):
+        ensino_medio=validated_data.get('ra') is None
+        #validated_data['ensino_meido'] = ensino_medio
+
         user = User(
             cpf = validated_data['cpf'],
             email = validated_data['email'],
-            name=validated_data['name'],
-            ensino_medio=validated_data.get('ensino_medio',False)
+            name = validated_data['name'],
+            ra = validated_data.get('ra'),
+            ensino_medio = ensino_medio
         )
         user.password = make_password(validated_data['password'])
         user.save()
--- api_django/urls.py
+++ api_django/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import user_views,area_views,course_views,organization_views
+from .views.user_views import UserTemplate
 
 urlpatterns = [
    path('users/',user_views.UserManager.as_view()),
@@ -9,5 +10,6 @@ urlpatterns = [
    path('courses/',course_views.CourseManager.as_view()),
    path('areas/',area_views.AreaManager.as_view()),
    path('organizations/',organization_views.OrganizationManager.as_view()),
-   path('email/',user_views.SendVerificationEmail.as_view())
+   path('email/',user_views.SendVerificationEmail.as_view()),
+   path('main/',UserTemplate.user_list,name="main")
 ]
